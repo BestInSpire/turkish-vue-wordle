a563deb95cb540b24aa881153155ff776bcfaf35
Özgür Öztürk <56519674+BestInSpire@users.noreply.github.com>
2022-01-31 20:44:59 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export const enum LetterState {
   INITIAL = 0,
-  CORRECT = 'doğru',
-  PRESENT = 'yeri yanlış',
-  ABSENT = 'yok'
+  CORRECT = 'correct',
+  PRESENT = 'present',
+  ABSENT = 'absent'
 }
